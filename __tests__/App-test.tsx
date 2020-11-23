4e6981b739015cb12c12f0c9e3ace085d5f56e44
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2020-11-23 01:07:08 -0300

    test: setup tests

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,19 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer, { act } from 'react-test-renderer';
 
-it('renders correctly', () => {
-	renderer.create(<App />);
+jest.mock('@react-navigation/native', () => {
+	return {
+		useNavigation: jest.fn(),
+	};
+});
+
+describe('Home', () => {
+	it('renders correctly', async () => {
+		await act(async () => {
+			renderer.create(<App />);
+		});
+	});
 });
